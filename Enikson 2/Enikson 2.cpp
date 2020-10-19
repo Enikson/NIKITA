f4fd6882e9bb384f9ffc1218e53f1a380ff05a0a
Bloodies <eschepokov@mail.ru>
2020-10-19 20:35:38 +0500

    endl

--- Enikson 2/Enikson 2.cpp
+++ Enikson 2/Enikson 2.cpp
@@ -1,27 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	double x,y,z;
+	string input;
 
-	printf("Enter 1 : "); scanf("%lf",&x);
-	printf("Enter 2 : "); scanf("%lf",&y);
-	
-	z = x + y; 
-	
-	printf("%0.3lf + %0.3lf = %0.3lf \n",x,y,z);
-	
-	printf("Enter 1 : "); scanf("%lf",&x);
-	printf("Enter 2 : "); scanf("%lf",&y);
+	printf("Enter operator: ");	
+	cin >> input;
 
-	z = x - y;
 	
-	printf("%0.3lf + %0.3lf = %0.3lf \n",x,y,z);
-
-	z = x * y;
-
-	z = x / y;
-	return 0;
+	if (input == "+")
+	{
+		printf("Enter 1 number: ");	scanf_s("%lf", &x);
+		printf("Enter 2 number: ");	scanf_s("%lf", &y);
+		z = x + y;
+		printf("%0.3lf + %0.3lf = %0.3lf \n", x, y, z);
+	}
+	else if (input == "-")
+	{
+		printf("Enter 1 number: ");	scanf_s("%lf", &x);
+		printf("Enter 2 number: ");	scanf_s("%lf", &y);
+		z = x - y;
+		printf("%0.3lf - %0.3lf = %0.3lf \n", x, y, z);
+	}
+	else if (input == "/")
+	{
+		printf("Enter 1 number: ");	scanf_s("%lf", &x);
+		printf("Enter 2 number: ");	scanf_s("%lf", &y);
+		z = x / y;
+		printf("%0.3lf / %0.3lf = %0.3lf \n", x, y, z);
+	}
+	else if (input == "*")
+	{
+		printf("Enter 1 number: ");	scanf_s("%lf", &x);
+		printf("Enter 2 number: ");	scanf_s("%lf", &y);
+		z = x * y;
+		printf("%0.3lf * %0.3lf = %0.3lf \n", x, y, z);
+	}
+	else if (input == "sin")
+	{
+		printf("Enter number: ");	scanf_s("%lf", &x);
+		z = sin(x);
+		printf("sin %0.3lf = %0.3lf \n", x, z);
+	}
+	else if (input == "sqrt")
+	{
+		printf("Enter number: ");	scanf_s("%lf", &x);
+		z = sqrt(x);
+		printf("sqrt %0.3lf = %0.3lf \n", x, z);
+	}
+	else
+	{
+		printf("error input");
+	}
+	
+	string restart;
+	printf("¬вести заново? y/n\n");
+	cin >> restart;
+	if (restart == "y")
+	{
+		main();
+	}
+	else
+	{
+		exit(0);
+	}
 }
